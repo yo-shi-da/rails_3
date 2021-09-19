f260ac266943156a8fb3606c010025e0d290b167
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-09-19 13:49:34 +0900

    heroku対応11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'hidden-lowlands-57035.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'boiling-waters-17073.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
